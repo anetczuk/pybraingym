7ccfd3ea788e0d4247f26ad90fd3245685793ab4
anetczuk <anetczuk@o2.pl>
2019-07-14 00:54:17 +0200

    Update script

--- startenv.sh
+++ startenv.sh
@@ -2,7 +2,22 @@
 
 set -eu
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+
+tmpfile=$(mktemp start.venv.XXXXXX.sh --tmpdir)
+
+
+cat > $tmpfile <<EOL
+source $SCRIPT_DIR/venv/bin/activate
+if [ \$? -ne 0 ]; then
+    echo -e "Unable to activate virtual environment, exiting"
+    exit 1
+fi 
+exec </dev/tty 
+EOL
+
 
 echo "Starting virtual env"
 
-bash -i <<< 'source venv/bin/activate; exec </dev/tty'
+bash -i <<< "source $tmpfile"
